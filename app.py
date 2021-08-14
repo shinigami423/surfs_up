39eb898383371f2f629d5d60d3525a44b12ccc9e
Daniel Chu <daniel930423@yahoo.com>
2021-08-14 10:33:37 -0700

    uploaded challenge and updated files

--- app.py
+++ app.py
@@ -34,11 +34,11 @@ app = Flask(__name__)
 def welcome():
     return(
     '''
-    Welcome to the Climate Analysis API!
-    Available Routes:
-    /api/v1.0/precipitation
-    /api/v1.0/stations
-    /api/v1.0/tobs
+    Welcome to the Climate Analysis API!<br/>
+    Available Routes:<br/>
+    /api/v1.0/precipitation<br/>
+    /api/v1.0/stations<br/>
+    /api/v1.0/tobs<br/>
     /api/v1.0/temp/start/end
     ''')
 
